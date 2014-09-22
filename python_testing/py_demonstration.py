97d9bbf4f4a5201bb12fbcb66097dc98246177fe
crocanura <lennonwotzke@hotmail.com>
2014-09-22 15:47:08 -0700

    added python script

---
+++ python_testing/py_demonstration.py
@@ -0,0 +1,17 @@
+from random import randint
+from time import sleep
+print ("running death clock\n\n")
+sleep(5)
+print ("please enter sudo password for Lennon\n")
+sleep(10)
+print ("fatal exception 0x00456292\n\nsystem is going down for halt NOW\n")
+sleep(5)
+print ("error")
+sleep(1)
+print ("could not shut down computer\n")
+while(True):
+    tar = input("Good morning Dave\n")
+    if (tar == "Hello"):
+        break
+x = randint(0,101)
+print (x)
